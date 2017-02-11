e29519f7e45260f6c3f142f1960d0a5f29cce316
Jouni Roivas <jroivas@iki.fi>
2017-02-12 01:52:21 +0200

    cdr, car approach

--- lisp.py
+++ lisp.py
@@ -64,6 +64,81 @@ def parse(prg):
     res, partials = appendPartials(res, partials)
     return res, idx
 
+def parseAtom(val, cdr, car):
+    val = atom(val)
+    if val is not None and val != '':
+        if cdr is None:
+            cdr = val
+        elif car is None:
+            car = val
+        else:
+            car = (car, val)
+    val = ''
+
+    return (val, cdr, car)
+
+def parsePair(prg):
+    res = []
+    partials = ''
+    idx = 0
+    dest = len(prg)
+    cdr = None
+    car = None
+    while idx < dest:
+        c = prg[idx]
+        #print ( '+cc %s %s %s' % (c,res, idx))
+        if c == '"' or (partials and partials[0] == '"'):
+            if partials:
+                if partials[0] == '"':
+                    partials += c
+                if c == '"':
+                    res, partials = appendPartials(res, partials)
+                    partials = ''
+            else:
+                partials = '"'
+            idx += 1
+        elif c == ' ':
+            #res, partials = appendPartials(res, partials)
+            partials, cdr, car = parseAtom(partials, cdr, car)
+            #print ('GGP %s, %s' % (cdr, car))
+            #partials = ''
+            #print ('GGG %s,%s'% (res, partials))
+            idx += 1
+        elif c == '(':
+            partials, cdr, car = parseAtom(partials, cdr, car)
+            #print ('PPP %s, %s' % (cdr, car))
+            #res, partials = appendPartials(res, partials)
+            #print ('PPP %s,%s'% (res, partials))
+            rest = prg[idx + 1:]
+            #a, b = parsePair(rest)
+            #idx += b + 1
+            a, b, c = parsePair(rest)
+            idx += c + 1
+            if cdr is None:
+                cdr = a
+                car = b
+            elif car is None:
+                car = (a, b)
+            else:
+                car = (car, (a, b))
+            #res.append({'data': a})
+            #res.append(a)
+        elif c == ')':
+            #res, partials = appendPartials(res, partials)
+            partials, cdr, car = parseAtom(partials, cdr, car)
+            #print ('RRR %s,%s'% (res, partials))
+            #print ('RRP %s, %s' % (cdr, car))
+            return (cdr, car, idx + 1)
+            #return res, idx + 1
+        else:
+            partials += c
+            idx += 1
+
+    #res, partials = appendPartials(res, partials)
+    partials, cdr, car = parseAtom(partials, cdr, car)
+    return (cdr, car, idx)
+    #return res, idx
+
 class LambdaCall(object):
     def __init__(self, body, params, env):
         self.params = params
@@ -151,10 +226,64 @@ def evaluate(item, env, dp=0):
         p = res(*args)
         return p
 
+def stackEval(val, env):
+    print ('SE', val)
+    """
+    if type(cdr) == tuple:
+        print ('RR', cdr)
+    """
+    if type(val) == tuple:
+        cdr, car = val
+        print ('St', cdr, car)
+        if type(cdr) == tuple:
+            l = stackEval(cdr, env)
+            r = stackEval(car, env)
+            if l is not None and r is not None:
+                return (l, r) 
+                #return l
+                #raise ValueError('Unexpected')
+            elif r is not None:
+                return r
+        elif cdr == 'begin':
+            return stackEval(car, env)
+        elif cdr == 'define':
+            if type(car) == tuple:
+                var = car[0]
+                if type(var) == tuple:
+                    var = var[0]
+                    args = var[1]
+                    #print ('DDD', stackEval(var, env))
+                    print ('DDD', var, args)
+                else:
+                    val = stackEval(car[1], env)
+                    env[var] = val
+                #stackEval(car[1], env)
+            else:
+                raise ValueError('Unexpected define: %s' % val)
+        elif cdr == '*':
+            l = stackEval(car[0], env)
+            r = stackEval(car[1], env)
+            #print ('MMM', l, r)
+            return l * r
+        else:
+            return val
+            #raise ValueError('Unexpected tuple: %s' % str(val))
+        #print (stackEval(car, env))
+    elif type(val) == int or type(val) == float:
+        return val
+    elif type(val) == str:
+        v = env.find(val)
+        if v is None:
+            raise ValueError('Undefined variable: %s' % val)
+        return v
+    else:
+        raise ValueError('Unexpected entry: %s' % val)
+
 if __name__ == '__main__':
     data = readfile(sys.argv[1])
     #print (data)
-    re, pos = parse(data)
+    cdr, car, pos = parsePair(data)
+    prg = (cdr, car)
     #print (re)
     env = Env()
     env.update(vars(math))
@@ -169,4 +298,6 @@ if __name__ == '__main__':
         '/': operator.truediv,
         'none': None
     })
-    print (evaluate(re, env))
+    #print (evaluate(re, env))
+    print (' Rp ', prg)
+    print (stackEval(prg, env))
