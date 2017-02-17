5e5af5b63753f1f53c99b18ad06eacfecdb06c0a
Jouni Roivas <jroivas@iki.fi>
2017-02-17 20:25:15 +0200

    Fix example

--- examples/fib3.lisp
+++ examples/fib3.lisp
@@ -1,6 +1,6 @@
 (define (fibonacci N)
   "Compute the N'th Fibonacci number."
-  (def b 10)
+  (define b 10)
   (if (or (equal? N 0) (= N 1))
       1
     (+ (fibonacci (- N 1)) (fibonacci (- N 2)))))
