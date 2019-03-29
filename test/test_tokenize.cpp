f70e2174fdd84f70983f5dd593ff53c106276232
Jouni Roivas <jroivas@iki.fi>
2019-03-29 18:21:09 +0200

    Test whitespace

--- test/test_tokenize.cpp
+++ test/test_tokenize.cpp
@@ -40,3 +40,33 @@ TEST_CASE("Tokenize list inside list", "[token]") {
     REQUIRE(t.next() == ")");
     REQUIRE(t.eof());
 }
+
+TEST_CASE("Tokenize peek", "[token]") {
+    Tokenize t("(+ 1 2)");
+    REQUIRE(!t.eof());
+    REQUIRE(t.next() == "(");
+    REQUIRE(t.peek() == "+");
+    REQUIRE(t.peek() == "+");
+    REQUIRE(t.next() == "+");
+    REQUIRE(t.next() == "1");
+    REQUIRE(!t.eof());
+}
+
+TEST_CASE("Don't tokenize comment", "[token]") {
+    Tokenize t("1 2 ; comment");
+    REQUIRE(!t.eof());
+    REQUIRE(t.next() == "1");
+    REQUIRE(t.next() == "2");
+    REQUIRE(t.eof());
+}
+
+TEST_CASE("Tokenize white space", "[token]") {
+    Tokenize t("    (   1              2     )");
+    REQUIRE(!t.eof());
+    REQUIRE(t.next() == "(");
+    REQUIRE(t.next() == "1");
+    REQUIRE(t.next() == "2");
+    REQUIRE(t.next() == ")");
+    REQUIRE(t.eof());
+}
+
