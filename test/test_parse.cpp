d05c0bd75167913857f0ed4639cd5a36b87f123c
Jouni Roivas <jroivas@iki.fi>
2019-03-30 21:00:05 +0200

    Test int

--- test/test_parse.cpp
+++ test/test_parse.cpp
@@ -18,6 +18,15 @@ TEST_CASE("Parse string", "[parse]") {
     REQUIRE(toString(v)->value() == "ab");
 }
 
+TEST_CASE("Parse int", "[parse]") {
+    Tokenize t("42");
+    Parse p(t);
+    Value *v = p.readForm();
+    REQUIRE(v != nullptr);
+    REQUIRE(v->type() == Type::Int);
+    REQUIRE(toInt(v)->value() == 42);
+}
+
 TEST_CASE("Parse empty list", "[parse]") {
     Tokenize t("()");
     Parse p(t);
