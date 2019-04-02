0ee05b55da529a80199bd8ccf143469a30108500
Jouni Roivas <jroivas@iki.fi>
2019-04-02 20:59:41 +0300

    Utilize Catch Me2 instead of Catch2

--- test/update_catch.sh
+++ test/update_catch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 MYDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-curl -s "https://raw.githubusercontent.com/catchorg/Catch2/master/single_include/catch2/catch.hpp" -o "${MYDIR}/catch.hpp"
+curl -s "https://raw.githubusercontent.com/jroivas/catch-me2-if-you-can/master/include/catch-me2.hpp" -o "${MYDIR}/catch.hpp"
