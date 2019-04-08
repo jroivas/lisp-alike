74f07b89b5d408f7fc40e6ca1908ca9b025b61da
Jouni Roivas <jroivas@iki.fi>
2019-04-08 19:15:18 +0300

    Way to compare output of interpreter

---
+++ test/cases.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -eu
+
+MYDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+binary=$1
+
+find "${MYDIR}/cases/"*.out | while read f; do
+    b=$(basename "$f" .out)
+    if diff -u <("${binary}" < "${MYDIR}/cases/$b") "$f"; then
+        echo "^^ ERROR test case caused faulty output: $b"
+    fi
+done
